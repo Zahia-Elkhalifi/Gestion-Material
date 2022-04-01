edbbdc29bdad3659d699c092dae902aa6d41f7a8
zahia elkhalifi <82174791+Zahia-Elkhalifi@users.noreply.github.com>
2022-04-02 01:40:38 +0200

    Add files via upload

---
+++ src/com/ensa/gi4/datatabase/Db.java
@@ -0,0 +1,9 @@
+package com.ensa.gi4.datatabase;
+
+import com.ensa.gi4.modele.Materiel;
+
+import java.util.ArrayList;
+
+public class Db {
+    public ArrayList<Materiel> materiels = new ArrayList<Materiel>();
+}
